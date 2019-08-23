d4960188e1d82767d602d8a884958e4d23f6423b
Christophe van Baalen <chichoch@gmail.com>
2019-08-23 10:09:41 +0200

    init

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {CameraExample} from "./src/CameraExample";
+
+const App = () => {
+
+    return (
+        <CameraExample/>
+    );
+};
+
+export default App;
