d65231e3ec04d3388a5e5ba8127afe085101a9eb
Christophe van Baalen <chichoch@gmail.com>
2019-08-23 12:00:05 +0200

    Can now save video to cameraRolL

--- src/CameraExample.tsx
+++ src/CameraExample.tsx
@@ -1,6 +1,8 @@
-import React, { PureComponent } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React, {PureComponent} from 'react';
+import {Alert, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
+
+import {RNCamera} from 'react-native-camera';
 
 const PendingView = () => (
     <View
@@ -15,14 +17,27 @@ const PendingView = () => (
     </View>
 );
 
+interface State {
+    isRecording: boolean;
+    videoUri: string;
+    hasRecorded: boolean;
+}
+
 export class CameraExample extends PureComponent {
+    state = {
+        isRecording: false,
+        videoUri: '',
+    };
+
+
     render() {
+        const buttonText = this.state.isRecording ? 'STOP' : 'START';
         return (
             <View style={styles.container}>
                 <RNCamera
                     style={styles.preview}
                     type={RNCamera.Constants.Type.back}
-                    flashMode={RNCamera.Constants.FlashMode.on}
+
                     androidCameraPermissionOptions={{
                         title: 'Permission to use camera',
                         message: 'We need your permission to use your camera',
@@ -36,12 +51,12 @@ export class CameraExample extends PureComponent {
                         buttonNegative: 'Cancel',
                     }}
                 >
-                    {({ camera, status, recordAudioPermissionStatus }) => {
-                        if (status !== 'READY') return <PendingView />;
+                    {({camera, status, recordAudioPermissionStatus}) => {
+                        if (status !== 'READY') return <PendingView/>;
                         return (
-                            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                                <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
-                                    <Text style={{ fontSize: 14 }}> SNAP </Text>
+                            <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+                                <TouchableOpacity onPress={() => this.takeVideo(camera)} style={styles.capture}>
+                                    <Text style={{fontSize: 14}}> {buttonText} </Text>
                                 </TouchableOpacity>
                             </View>
                         );
@@ -51,8 +66,36 @@ export class CameraExample extends PureComponent {
         );
     }
 
-    takePicture = async function(camera: RNCamera) {
-        const options = { quality: 0.5, base64: true };
+    takeVideo(camera: RNCamera) {
+        const uri = this.state.videoUri;
+        this.state.isRecording ? this.stopRecording(camera).catch(x => console.log('SAVE ERROR!', x)) : this.startRecording(camera);
+        this.setState({
+            isRecording: !this.state.isRecording,
+        });
+    }
+
+    startRecording = async function (camera: RNCamera) {
+        const data = await camera.recordAsync()
+            .then(data => CameraRoll.saveToCameraRoll(data.uri))
+            .then(() => Alert.alert('Success', 'Video saved!'))
+            .catch(e => console.log('ERROR', e));
+        console.log('Saved to camera roll:', data);
+        return data;
+    };
+
+    stopRecording = async function (camera: RNCamera) {
+        const data = await camera.stopRecording();
+        console.log('Stopped recording');
+        return;
+    };
+
+    saveVideoToCameraRoll(uri: string) {
+        const data = CameraRoll.saveToCameraRoll(uri, 'video');
+        console.log('Saved to camera roll:', uri);
+    }
+
+    takePicture = async function (camera: RNCamera) {
+        const options = {quality: 0.5, base64: true};
         const data = await camera.takePictureAsync(options);
         //  eslint-disable-next-line
         console.log(data.uri);
