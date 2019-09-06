0266c7eea963ce88b742a709b3e2a3774f7a74c2
Christophe van Baalen <chichoch@gmail.com>
2019-09-06 16:22:34 +0200

    WIP: zoom

--- src/CameraExample.tsx
+++ src/CameraExample.tsx
@@ -1,15 +1,60 @@
 import React, {PureComponent} from 'react';
-import {Alert, StyleSheet, Text, View} from 'react-native';
+import {Alert, Animated, Easing, PanResponder, PanResponderInstance, StyleSheet, Text, View} from 'react-native';
 import CameraRoll from '@react-native-community/cameraroll';
 
 import {RNCamera} from 'react-native-camera';
 import {ButtonComponent} from "./ButtonComponent";
 import {PendingView} from "./PendingView";
-import {PinchGestureHandler, PinchGestureHandlerGestureEvent,} from 'react-native-gesture-handler';
+import {PinchGestureHandlerGestureEvent,} from 'react-native-gesture-handler';
 import Orientation from "react-native-orientation-locker";
 
 export class CameraExample extends PureComponent {
+  private _panResponder: PanResponderInstance;
+  private _animatedZoom: Animated.Value;
+  _zoom: number;
+
+  constructor(props: any) {
+    super(props);
+    this._panResponder = PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+        this.onZoom(gestureState.dy);
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true;
+      },
+    });
+    this._animatedZoom = new Animated.Value(0);
+    this._zoom = 0;
+    this._animatedZoom.addListener((x) => this._zoom = x.value);
+  }
+
   pinchRef = React.createRef();
+
   state = {
     isRecording: false,
     videoUri: '',
@@ -18,6 +63,18 @@ export class CameraExample extends PureComponent {
     debug: 'TEST',
     orientation: 'PORTRAIT',
   };
+
+  onZoom(distance: number) {
+    let newZoom = this.state.zoom + (distance / 100000);
+    if (newZoom > 1) {
+      newZoom = 1;
+    } else if (newZoom < 0) {
+      newZoom = 0;
+    }
+    this._animatedZoom.setValue(newZoom);
+
+    //this.setState({zoom: newZoom})
+  }
 
   onGestureHandler(event: PinchGestureHandlerGestureEvent) {
     let newZoom = this.state.zoom + (event.nativeEvent.velocity / 1500);
@@ -45,7 +102,8 @@ export class CameraExample extends PureComponent {
   orientationDidChange = (orientation: string) => {
     console.log(orientation);
     if (orientation !== 'UNKNOWN' && !this.state.isRecording) {
-      this.setState({orientation})
+      this.setState({orientation});
+      this.forceUpdate();
     }
   };
 
@@ -57,7 +115,7 @@ export class CameraExample extends PureComponent {
           <RNCamera
             style={styles.preview}
             type={RNCamera.Constants.Type.back}
-            zoom={zoom}
+            zoom={this._zoom}
             defaultVideoQuality={RNCamera.Constants.VideoQuality["1080p"]}
             androidCameraPermissionOptions={{
               title: 'Permission to use camera',
@@ -76,8 +134,9 @@ export class CameraExample extends PureComponent {
               if (status !== 'READY') return <PendingView/>;
               return (
                 <View style={{flex: 1, width: '100%'}}>
-                  <PinchGestureHandler
-                    onGestureEvent={(x: PinchGestureHandlerGestureEvent) => this.onGestureHandler(x)}
+                  <View
+                    style={{flex: 1, opacity: 0.5, backgroundColor: 'blue'}}
+                    {...this._panResponder.panHandlers}
                   >
                     <View style={{flex: 1, justifyContent: 'flex-end'}}>
                       <Text>{this.state.debug}</Text>
@@ -92,7 +151,7 @@ export class CameraExample extends PureComponent {
                         orientation={orientation}
                       />
                     </View>
-                  </PinchGestureHandler>
+                  </View>
                 </View>
               )
             }}
@@ -121,9 +180,15 @@ export class CameraExample extends PureComponent {
 
   startRecording = async function (camera: RNCamera) {
     console.log('Started recording');
+    /*
+    const recordOptions = {
+      codec:
+    };
+    */
     const data = await camera.recordAsync()
       .then(x => {
         console.log('Finished recording', x.uri);
+        console.log('Video codec', x.codec);
         return x.uri;
       })
       .catch(e => console.log('ERROR', e));
@@ -156,7 +221,7 @@ const styles = StyleSheet.create({
   bounding: {
     flex: 0,
     width: '100%',
-    aspectRatio: 9/16, // TODO Needs to work
+    aspectRatio: 9 / 16, // TODO Needs to work
   },
   preview: {
     flex: 1,
